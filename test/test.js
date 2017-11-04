a1772b67bee8b443df98b76d38d6187d9666075b
Pieter Huibers <pieterhuibers@gmail.com>
2017-11-04 09:57:52 +0100

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,13 @@
+// Simplle async test for HTTP 200 response code using supertest
+'use strict';
+
+var request = require('supertest'),
+		app = require('../app').getApp;
+
+describe('GET /', function() {
+	it('expects HTPP response 200', function(done) {
+		request(app)
+			.get('/')
+			.expect(200, done);
+	});
+});
